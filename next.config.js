dffccfc4363b6604c483d2ab40323541c0472dca
shadowhacker8 <happyemilliano@gmail.com>
2025-06-17 20:49:10 +0100

    Fix Next.js Configuration and Start Development Server

--- next.config.js
+++ next.config.js
@@ -5,28 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  swcMinify: false,
   experimental: {
-    forceSwcTransforms: false,
-    esmExternals: false
-  },
-  webpack: (config, { isServer }) => {
-    // Disable SWC completely and use Babel for transforms
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-    
-    return config;
+    optimizeCss: true
   },
   compiler: {
-    removeConsole: false
+    removeConsole: process.env.NODE_ENV === 'production'
   }
 }
 
